ee079bf983a456fa43869d162d3540110deb086c
Simon Brummer <simon.brummer@gmail.com>
2022-10-27 14:17:12 -0400

    Initial project setup

---
+++ src/main/java/com/brummer/investmenttracker/InvestmentTracker/InvestmentTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.brummer.investmenttracker.InvestmentTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentTrackerApplication.class, args);
+	}
+
+}
